bdd63e4d1fb6d452f4d995edded688161b9c07d7
VedantAnand17 <vedantanand0077@gmail.com>
2024-07-05 16:02:13 +0530

    completed routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { createTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 
 app.use(express.json());
 
-app.get('/todo', (req, res) => {
+app.get('/todo',async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload)
     if (!parsedPayload.success) {
@@ -14,13 +15,22 @@ app.get('/todo', (req, res) => {
         return;
     }
     // putting in mongodb
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "todo created"
+    })
 });
 
 app.get('todos', (req, res) => {
-
+    const todos = todo.find({});
+    res.json(todos);
 });
 
-app.post('/comleted', () => {
+app.post('/comleted', async () => {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload)
     if (!parsedPayload.success) {
@@ -29,6 +39,14 @@ app.post('/comleted', () => {
         })
         return;
     }
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed: true
+    })
+    res.json({
+        msg: "Todo completed"
+    })
 });
 
 app.listen(3000);
